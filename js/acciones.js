1eba867f55005bd517bb0fdf3e8b216231e008cc
aGomez <aGomez>
2013-11-21 15:45:10 -0600

    subiendo4

--- js/acciones.js
+++ js/acciones.js
@@ -2,33 +2,43 @@
 
 
 $(document).ready(function(e){
-var db=OpenDataBase("Test","1.0","Test",65535);
 	document.addEventListener("deviceready",function(){
-$('#Crear').bind("Click",function(event){
+var db=OpenDataBase("Test","1.0","Test",65535);
+
+
+	
+$('#Crear').bind("click",function(event){
 	db.transaction(function(ejecutar) {
 	var SQL="CREATE TABLE Clientes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(64) NOT NULL, apellido VARCHAR(100))"			
 
 ejecutar.executeSQL(SQL,undefined, function() {				
 
-navigator.notification.alert("Tabla Creada");
+alert("Tabla Creada");
 
    }, error); 
    });//ejecutar 
    });//click crear		
    
-$('#Eliminar').bind("Click",function(event){
-	if(!navigator.notification.confirm("Borrar Tabla ",""))
-	return;
+$('#Eliminar').bind("click",function(event){
+	if(!confirm("Borrar Tabla ",""))return;;
 	db.transaction(function(ejecutar) {
 	var SQL="DROP TABLE Clientes";
 	ejecutar.executeSQL(SQL,undefined, function() {				
-    navigator.notification.alert("Tabla Borrada");
+    alert("Tabla Borrada");
 
    }, error); 
    });//ejecutar 
    });//click eliminar 
 
+function error (ejecutar, err) 
+{
+  alert ("Error de Base de Datos : " + err.message);
+  return false;
+}    
+
 },false);//ready device
+
+
 	
 	
 });//document
