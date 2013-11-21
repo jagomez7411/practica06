86f14a7040b3b490f1a1564e83645e93dcf3bd46
aGomez <aGomez>
2013-11-21 16:24:17 -0600

    sub8

--- js/acciones.js
+++ js/acciones.js
@@ -2,14 +2,13 @@
 
 
 $(document).ready(function(e){
-alert("Listo");
 	document.addEventListener("deviceready",function(){
 var db=openDatabase("Test","1.0","Test",65535);
 
 
 	
 $('#Crear').bind("click",function(event){
-	alert("crear");
+
 	db.transaction(function(ejecutar) {
 	var SQL="CREATE TABLE Clientes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(64) NOT NULL, apellido VARCHAR(100))"			
 
@@ -22,7 +21,7 @@ alert("Tabla Creada");
    });//click crear		
    
 $('#Eliminar').bind("click",function(event){
-	alert("borrara");
+
 	if(!confirm("Borrar Tabla ",""))return;
 	db.transaction(function(ejecutar) {
 	var SQL="DROP TABLE Clientes";
